3b1e7613113d8e7319037b473173fba3d91655f4
pavan kumar <pavank690@gmail.com>
2016-09-03 23:41:58 +0530

    string reversing

---
+++ strrev.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	char s[20];
+	printf("enter a string\n");
+	scanf("%s",,&s);
+	s=strrev(s);
+	printf("revesed string is "%s"",s);
+	return 0;
+}
